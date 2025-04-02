35541a2666b62190ebf86c9847092b950394a243
Superstar.W <yafei@YafeiMacBook-Air.local>
2025-04-03 00:09:15 +1100

    update the notification logic

--- frontend/src/notification.js
+++ frontend/src/notification.js
@@ -4,10 +4,29 @@
  * @param {'info' | 'success' | 'warning' | 'error'} type - Type of the notification.
  * @param {number} duration - Duration in ms before auto-hide (default 5000ms).
  */
-export function showNotification(message, type = 'info', duration = 3000) {
-    const container = document.getElementById('notification-container');
-    if (container.children.length >= 5) return;
+let lastNotificationCache = {
+    message: '',
+    type: '',
+    timestamp: 0
+};
 
+export function showNotification(message, type = 'info', duration = 3000) {
+    const now = Date.now();
+    const container = document.getElementById('notification-container');
+
+    // If the message is repeated and within 3 seconds, it is skipped
+    if (
+        message === lastNotificationCache.message &&
+        type === lastNotificationCache.type &&
+        now - lastNotificationCache.timestamp < 1000
+    ) {
+        return;
+    }
+
+    lastNotificationCache = { message, type, timestamp: now };
+
+    // Maximum 5 error messages
+    if (container.children.length >= 5) return;
     // Create container
     const notif = document.createElement('div');
     notif.classList.add('notification', `notification-${type}`);
